553b83eaafb241f16257387852b38edd95c6f16b
Sadrach Juan Diego Garcia Flores <Jdiego0805@gmail.com>
2025-01-29 13:34:48 -0700

    se cambio el QuestionsA a statefulwidget

--- lib/questionsa.dart
+++ lib/questionsa.dart
@@ -1,25 +1,63 @@
 import 'package:flutter/material.dart';
 
-class QuestionsAPage extends StatelessWidget {
+class QuestionsAPage extends StatefulWidget {
   final Function(int) onPageChanged;
 
   const QuestionsAPage({super.key, required this.onPageChanged});
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Questionsa',
-              style: TextStyle(fontSize: 40),
+  State<QuestionsAPage> createState() => _QuestionsAPageState();
+}
+
+  class _QuestionsAPageState extends State<QuestionsAPage> {
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+    @override
+    Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: TextField(
+              decoration: InputDecoration(hintText: "Cual es tu nombre?"),
             ),
-            ElevatedButton(
-              onPressed: () => {onPageChanged(3)} , child: const Text('continuar'),
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: TextField(
+              decoration: InputDecoration(hintText: "Cual es tu telefono?"),
             ),
-          ],
-        ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: TextField(
+              decoration: InputDecoration(hintText: "Cual es tu correo?"),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: TextField(
+              decoration: InputDecoration(hintText: "Cual es tu matricula?"),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: TextField(
+              decoration: InputDecoration(hintText: "Cual es tu edad?"),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () => {widget.onPageChanged(3)},
+            child: const Text('continuar'),
+          ),
+        ],
+      ),
     );
   }
 }
